0a5a8b6d2ca2d8c8fe9ed1e7f47e4c767e7737d8
dongxiaohong <dongxiaohong@daokoudai.com>
2018-12-14 16:48:07 +0800

    java init

---
+++ src/main/java/com/asm/jelly/JellyAsmBoot.java
@@ -0,0 +1,12 @@
+package com.asm.jelly;
+
+/**
+ * @author dongxiaohong
+ * @date 2018/12/14 16:47
+ */
+public class JellyAsmBoot {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/asm/jelly/JellyAsmBootTest.java
@@ -0,0 +1,14 @@
+package com.asm.jelly;
+
+import org.junit.Test;
+
+/**
+ * @author dongxiaohong
+ * @date 2018/12/14 16:47
+ */
+public class JellyAsmBootTest {
+    @Test
+    public void test01(){
+
+    }
+}
